8fbfc21e52367491f669ea53bbda0cd9ac9f625a
Dhruv-sengar <167276533+Dhruv-sengar@users.noreply.github.com>
2024-04-27 22:07:10 +0530

    voting program

---
+++ voting.py
@@ -0,0 +1,13 @@
+def vote_yours():
+        a = input("ENTER YOUR CHOICE: ")
+        if a == "BJP":
+                print(f"YOUR VOTE IS ALLOTTED TO {a}")
+        elif a == "CONGRESS":
+                print(f"YOUR VOTE IS ALLOTTED TO {a}")
+        elif a == "AAP":
+                print(f"YOUR VOTE IS ALLOTTED TO {a}")
+        elif a == "SP":
+                print(f"YOUR VOTE IS ALLOTTED TO {a}")
+        else:
+                print(f"YOUR VOTE IS ALLOTTED TO {a}")
+vote_yours()
